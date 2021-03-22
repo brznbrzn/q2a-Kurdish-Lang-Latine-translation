d0929fada5e1c3f8ad7a62872692ff5d85a3407a
brznbrzn <67524782+brznbrzn@users.noreply.github.com>
2021-03-22 16:41:07 +0300

    Add files via upload

---
+++ qa-lang-profile.php
@@ -0,0 +1,90 @@
+<?php
+/*
+	Question2Answer by Gideon Greenspan and contributors
+	http://www.question2answer.org/ & http://www.turksdesk.org
+
+	Description: Language phrases commonly used throughout Q2A
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	return array(
+		'1_chosen_as_best' => ' (1 wek Bersiva Baştirîn hate hate hilbijartin)',
+		'1_down_vote' => '1 dengê nerênî',
+		'1_up_vote' => '1 dengê erênî',
+		'1_with_best_chosen' => ' (1 bersivek wek ya baştirîn hate hilbijartin)',
+		'activity_by_x' => 'Hemû îstatîstîkên ^ ',
+		'answers' => 'Bersivê wê(î):',
+		'answers_by_x' => 'Bersivên ^',
+		'bonus_points' => 'Pûanên bonûs:',
+		'comments' => 'Şîroveyên wê(î):',
+		'delete_wall_post_popup' => 'Vî peyama profîlê jê bibe',
+		'extra_privileges' => 'Berjewendîyên extra:',
+		'gave_out' => 'Dengê ku daye:',
+		'my_account_title' => 'Agahîyên min',
+		'no_answers_by_x' => '^ vî gavê bersivê wê(î) tune',
+		'no_posts_by_x' => '^ vî gavê peyamê wê(î) tune',
+		'no_questions_by_x' => '^ vî gavê pirsê wê(î) tune ',
+		'permit_anon_view_ips' => '+Dikare (IP)ya peyamên kesên bê nav bibîne',
+		'permit_close_q' => '+Dikare pirsekî,ser daxwaza xwe bigre',
+		'permit_delete_hidden' => '+Dikare pirsên ku hatine veşartin jê bibe',
+		'permit_edit_a' => '+Dikare bersivekî li ser daxwaza xwe sererast bike',
+		'permit_edit_c' => '+Dikare şîrovekî li ser daxwaza xwe sererast bike',
+		'permit_edit_q' => '+Dikare pirsekî li ser daxwaza xwe sererast bike',
+		'permit_edit_silent' => '+Dikare peyamê P-B-Ş , li ser daxwaza xwe sererast bike',
+		'permit_flag' => '+Dikare peyama gilî bike',
+		'permit_hide_show' => '+Dikare peyamê P-B-Ş veşêre û dîsa bide nîşan',
+		'permit_moderate' => '+Dikare peyamê P-B-Ş ê erê bike ango red bike',
+		'permit_post_a' => '+Dikare bibersivîne',
+		'permit_post_c' => '+Dikare şîroveke',
+		'permit_post_q' => '+Dikare bipirse',
+		'permit_post_wall' => '+Dikare li ser profîla endama binvîse',
+		'permit_recat' => '+Dikare kategorîya pirsekî biguherîne',
+		'permit_retag' => '+Dikare taga pirsekî biguherîne',
+		'permit_select_a' => '+Dikare ji bo pirsekî , bersiva baştirîn hilbijêre',
+		'permit_view_new_users_page' => '+Dikare rûpela endamên nû bibîne',
+		'permit_view_q_page' => '+Dikare rûpela pirsa bibîne',
+		'permit_view_special_users_page' => '+Dikare rûpela endamên taybet bibîne',
+		'permit_view_voters_flaggers' => '+Dikare kesên ku deng dane ango gilî kirine bibîne',
+		'permit_vote_a' => '+Dikare deng bide ji bo bersiva',
+		'permit_vote_c' => '+Dikare deng bide ji bo şîroveya',
+		'permit_vote_down' => '+Dikare dengê nerênî bide',
+		'permit_vote_q' => '+Dikare deng bide ji bo pirsa',
+		'post_wall_blocked' => 'Ev kes destûra nivîsandinê a li ser profîla xwe nade',
+		'post_wall_button' => 'Peyamê bişîne',
+		'post_wall_empty' => 'Peyamekî ku tu dixwazî bişînî ser profîlê binvîse.(Ku tu vî kesî nasnakî nenvîse:))',
+		'post_wall_limit' => 'Ev profîl pir zêde , daxwaza nivîsandina profîlê hildaye.Lewra tu êdî nikarî binvîsî',
+		'post_wall_must_be_approved' => 'Ji bo peyam şandina li ser profîlê divê hesabê te bê erê kirin. Kerema xwe qasek bisekinin ango ^1agahîyên xwe sererast bike^2.',
+		'post_wall_must_confirm' => 'Ji bo peyam şandina li ser profîlê kerema xwe ^5emaîla xwe erê bike^6.',
+		'post_wall_must_login' => 'Ji bo peyam şandina li ser profîlê ^1Têkeve malperê^2 ango ^3Tomar bibin^4.',
+		'questions' => 'Pirsên wê(î):',
+		'questions_by_x' => 'Pirsên ^\'',
+		'ranked_x' => ' (Ev kes ^.\'mîne)',
+		'received' => 'Dengê ku hildaye:',
+		'recent_activity_by_x' => 'Hemû tevgerên ^',
+		'score' => 'Pûanên wê(î):',
+		'send_private_message' => ' - ^1Peyamêk bişîne^2',
+		'set_bonus_button' => 'Pûanê bonûs sererast bike',	
+		'title' => 'Extra:',
+		'user_x' => 'Endam: ^',
+		'user_x_disabled_pms' => '^ peyamên taybet hilnade. ',
+		'voted_on' => 'Dengên wê(î):',
+		'wall_for_x' => 'Profîla ^',
+		'wall_view_more' => 'Ji bo dîtina zêdetirîn peyam kerema xwe pêlivir bike...',
+		'x_chosen_as_best' => ' (^ peyamê wê(î) wek Bersivên Baştirîn hatîye hilbijartin.)',
+		'x_down_votes' => '^ dengê nerênî',
+		'x_up_votes' => '^ dengê erênî',
+		'x_with_best_chosen' => ' (^ wek bersiva baştirîn hate hilbijartin)',
+	    'delete_pm_popup' => 'Peyamê jê bibe',
+	);
